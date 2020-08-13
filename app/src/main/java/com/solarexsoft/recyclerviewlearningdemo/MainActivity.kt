32718c98e6edb3d0a18959611623e696678573ab
Solarex <rh.hou.work@gmail.com>
2020-08-13 11:51:47 +0800

    glide

--- app/src/main/java/com/solarexsoft/recyclerviewlearningdemo/MainActivity.kt
+++ app/src/main/java/com/solarexsoft/recyclerviewlearningdemo/MainActivity.kt
@@ -1,12 +1,104 @@
 package com.solarexsoft.recyclerviewlearningdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            loadContentUri()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if ((grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            loadContentUri()
+        }
+    }
+
+
+    @SuppressLint("CheckResult")
+    private fun loadContentUri() {
+        val rb1 = Glide.with(this).load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597298618864&di=065b0917ade52f7054d44164c902be3b&imgtype=0&src=http%3A%2F%2Fimg.meituan.net%2Favatar%2Fbf78c1bd3c7c569a87dbbe4d72ddc291103959.jpg")
+        rb1.addListener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.d(TAG, Thread.currentThread().name, e)
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.d(TAG, "rb1 ${Thread.currentThread().name} --> ${System.currentTimeMillis()}")
+                return false
+            }
+
+        })
+        rb1.into(iv1)
+        rb1.into(iv2)
+        val rb2 = Glide.with(this).load("content://media/external/images/media/61673")
+        rb2.addListener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.d(TAG, Thread.currentThread().name, e)
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.d(TAG, "rb2 ${Thread.currentThread().name} --> ${System.currentTimeMillis()}")
+                return false
+            }
+
+        })
+        rb2.into(iv3)
+        rb2.into(iv4)
     }
 }
